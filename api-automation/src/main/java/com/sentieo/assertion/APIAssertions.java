386fcb0f72811f8691e7bf49c2b52b52b7ba94f5
bhashkargaur <bhaskar@sentieo.com>
2019-09-27 23:01:19 +0530

    resolve compile error

--- api-automation/src/main/java/com/sentieo/assertion/APIAssertions.java
+++ api-automation/src/main/java/com/sentieo/assertion/APIAssertions.java
@@ -7,6 +7,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.json.JSONException;
 import org.testng.Assert;
