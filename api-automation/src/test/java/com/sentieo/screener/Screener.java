eb9a4c8acf8f57f3e00ddc19a3c4bd1cf80276fb
bhashkargaur <bhaskar@sentieo.com>
2019-11-29 13:18:35 +0530

    add data check in screener and comp

--- api-automation/src/test/java/com/sentieo/screener/Screener.java
+++ api-automation/src/test/java/com/sentieo/screener/Screener.java
@@ -2,6 +2,8 @@ package com.sentieo.screener;
 
 import static com.sentieo.constants.Constants.*;
 import java.util.HashMap;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -32,7 +34,7 @@ public class Screener extends APIDriver {
 
 		RestAssured.baseURI = USER_APP_URL;
 	}
-	
+
 	@BeforeMethod
 	public void setUp() {
 		verify = new APIAssertions();
@@ -111,27 +113,47 @@ public class Screener extends APIDriver {
 	@Test(groups = "sanity", description = "fetchscreenersearch")
 	public void fetchscreenersearch() throws Exception {
 		HashMap<String, String> queryParams = new HashMap<String, String>();
-		queryParams.put("source_fields",
-				"[\"name\",\"mos_unified_fq1_accel\",\"mos_gtrends_fq1_accel\",\"mos_alexa_uv_fq1_accel\",\"mos_gnip_fq1_accel\",\"mos_revenue_fq1_accel\",\"FQ_Q_S_enddt\",\"mos_unified_revenue_rsqd\",\"12-4-2018_D_R_LTCL-std####ltm\",\"12-4-2018_D_R_LBDT-std####ltm\",\"12-4-2018_D_R_LTXP-std####ltm\",\"12-16-2018_D_S_roic####ltm\",\"12-16-2018_D_S_gross_profit####ltm\",\"12-16-2018_D_S_QRED-std####ltm\",\"12-16-2018_D_S_opex_adj####ltm\",\"mos_tsentiments_management_FQ0_base\",\"12-16-2018_D_S_ebit_intexp####ltm\",\"12-16-2018_D_S_VDEP-std####ltm\",\"12-16-2018_D_S_APPY-std####ltm\",\"shper\",\"1d_RD_S_cps_est####ntm\",\"12-16-2018_D_S_ufcfmargin####ltm\",\"currency_stock\",\"1d_RD_S_price_cashflow####ltm\",\"1d_RD_S_p_fcf####ltm\",\"12-18-2018_D_S_SDWS####ltm\",\"1d_RD_S_ebitda-margin_est####cfy\",\"12-20-2018_D_S_taxes_adj####ltm\",\"yprice\",\"12-20-2018_D_S_quickratio####ltm\",\"12-22-2018_D_S_VDEP-std####ltm\",\"mos_unified_FQ2_yoy\",\"12-22-2018_D_S_arturn####ltm\",\"1d_RD_S_ev_fcf####ltm\",\"52wk_high\",\"12-26-2018_D_S_LCAV-std####ltm\",\"12-26-2018_D_S_shares_issue_3####ltm\",\"12-26-2018_D_S_APPY-std####ltm\",\"12-26-2018_D_S_LOCL-std####ltm\",\"12-26-2018_D_S_VRXP-std####ltm\",\"12-26-2018_D_S_ERES-std####ltm\",\"12-26-2018_D_S_ASTI-std####ltm\",\"1-1-2019_D_S_pre_tax_adj####ltm\",\"1-1-2019_D_S_shares_issue_3####ltm\",\"1-1-2019_D_S_FCSN####ltm\",\"1-1-2019_D_S_quickratio####ltm\"]");
-		queryParams.put("screener_search_settings", "{\"unique_str\":\"\",\"page_nav\":0,\"page_sort\":0}");
-		queryParams.put("screener_user_settings",
-				"{\"exclude_adrs\":false,\"exclude_otc_symbols\":false,\"exclude_non_primary\":false,\"exclude_adrs_non_primary\":false,\"exclude_non_cash_equity_securities\":false,\"exclude_inactive_tickers\":true}");
-		queryParams.put("currency", "cad");
+//		queryParams.put("source_fields",
+//				"[\"name\",\"mos_unified_fq1_accel\",\"mos_gtrends_fq1_accel\",\"mos_alexa_uv_fq1_accel\",\"mos_gnip_fq1_accel\",\"mos_revenue_fq1_accel\",\"FQ_Q_S_enddt\",\"mos_unified_revenue_rsqd\",\"12-4-2018_D_R_LTCL-std####ltm\",\"12-4-2018_D_R_LBDT-std####ltm\",\"12-4-2018_D_R_LTXP-std####ltm\",\"12-16-2018_D_S_roic####ltm\",\"12-16-2018_D_S_gross_profit####ltm\",\"12-16-2018_D_S_QRED-std####ltm\",\"12-16-2018_D_S_opex_adj####ltm\",\"mos_tsentiments_management_FQ0_base\",\"12-16-2018_D_S_ebit_intexp####ltm\",\"12-16-2018_D_S_VDEP-std####ltm\",\"12-16-2018_D_S_APPY-std####ltm\",\"shper\",\"1d_RD_S_cps_est####ntm\",\"12-16-2018_D_S_ufcfmargin####ltm\",\"currency_stock\",\"1d_RD_S_price_cashflow####ltm\",\"1d_RD_S_p_fcf####ltm\",\"12-18-2018_D_S_SDWS####ltm\",\"1d_RD_S_ebitda-margin_est####cfy\",\"12-20-2018_D_S_taxes_adj####ltm\",\"yprice\",\"12-20-2018_D_S_quickratio####ltm\",\"12-22-2018_D_S_VDEP-std####ltm\",\"mos_unified_FQ2_yoy\",\"12-22-2018_D_S_arturn####ltm\",\"1d_RD_S_ev_fcf####ltm\",\"52wk_high\",\"12-26-2018_D_S_LCAV-std####ltm\",\"12-26-2018_D_S_shares_issue_3####ltm\",\"12-26-2018_D_S_APPY-std####ltm\",\"12-26-2018_D_S_LOCL-std####ltm\",\"12-26-2018_D_S_VRXP-std####ltm\",\"12-26-2018_D_S_ERES-std####ltm\",\"12-26-2018_D_S_ASTI-std####ltm\",\"1-1-2019_D_S_pre_tax_adj####ltm\",\"1-1-2019_D_S_shares_issue_3####ltm\",\"1-1-2019_D_S_FCSN####ltm\",\"1-1-2019_D_S_quickratio####ltm\"]");
+//		queryParams.put("screener_search_settings", "{\"unique_str\":\"\",\"page_nav\":0,\"page_sort\":0}");
+//		queryParams.put("screener_user_settings",
+//				"{\"exclude_adrs\":false,\"exclude_otc_symbols\":false,\"exclude_non_primary\":false,\"exclude_adrs_non_primary\":false,\"exclude_non_cash_equity_securities\":false,\"exclude_inactive_tickers\":true}");
+//		queryParams.put("currency", "cad");
+//		queryParams.put("type", "company");
+//		queryParams.put("sort", "FY_Y_S_mkt_cap:desc");
+//		queryParams.put("fetchticker", "1");
+//		queryParams.put("pagetype", "custom");
+//		queryParams.put("screener_user_settings", "");
+
+		queryParams.put("tickers", "");
+		queryParams.put("pagetype", "screener");
 		queryParams.put("type", "company");
 		queryParams.put("sort", "FY_Y_S_mkt_cap:desc");
-		queryParams.put("fetchticker", "1");
-		queryParams.put("pagetype", "custom");
-		queryParams.put("screener_user_settings", "");
+		queryParams.put("currency", "usd");
+		queryParams.put("screener_search_settings", "{\"unique_str\":\"\",\"page_nav\":0,\"page_sort\":0}");
+
 		RequestSpecification spec = formParamsSpec(queryParams);
 
 		Response resp = RestOperationUtils.post(APP_URL + FETCH_SCREENER_SEARCH, null, spec, queryParams);
-		System.out.println(resp.asString());
 		APIResponse apiResp = new APIResponse(resp);
 		JSONObject respJson = new JSONObject(apiResp.getResponseAsString());
 		verify.verifyStatusCode(apiResp.getStatusCode(), 200);
 		verify.verifyResponseTime(resp, 5000);
 		verify.verifyEquals(respJson.getJSONObject("response").getBoolean("status"), true,
 				"Verify the API Response Status");
+		JSONObject values = respJson.getJSONObject("result");
+		JSONObject ticker_currency = values.getJSONObject("ticker_currency");
+		JSONArray ticker = values.getJSONArray("tickers");
+		JSONObject fields_info = values.getJSONObject("fields_info");
+		if (values.length() == 0)
+			verify.verifyTrue(false, "verify result : ");
+		if (ticker.length() == 0)
+			verify.verifyTrue(false, "verify tickers : ");
+		if (ticker_currency.length() == 0)
+			verify.verifyTrue(false, "verify ticker :  ");
+		if (fields_info.length() == 0)
+			verify.verifyTrue(false, "verify fields info :  ");
+
 		verify.verifyAll();
 	}
 }
