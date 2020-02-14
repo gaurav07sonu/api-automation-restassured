b554846dfea1ac45c3915149dcb02e6713323680
“sanjay-sentieo” <sanjay.saini@sentieo.com>
2020-02-14 13:26:02 +0530

    heart-beat changes

--- api-automation/src/test/java/com/sentieo/heartbeat/APIDriverHeartbeat.java
+++ api-automation/src/test/java/com/sentieo/heartbeat/APIDriverHeartbeat.java
@@ -165,12 +165,13 @@ public class APIDriverHeartbeat {
 		
 		sbFail.append("</tr>");
 		taggedUsers.add(users.get(team));
-		if(statusCode.equals("200")) {
-			failedAPIData.add(path.substring(path.lastIndexOf("api"), path.length()-1) + " | Data check failed: " + error + "\n");
-		}
-		else {
-			failedAPIData.add(path.substring(path.lastIndexOf("api"), path.length()-1) + " | Unexpected response code: " + statusCode + "\n");
-		}
+		failedAPIData.add(path.substring(path.lastIndexOf("api"), path.length()-1) + " ==> " + statusCode + "  ,  ");
+//		if(statusCode.equals("200")) {
+//			failedAPIData.add(path.substring(path.lastIndexOf("api"), path.length()-1) + " | Data check failed: " + error + "\n");
+//		}
+//		else {
+//			failedAPIData.add(path.substring(path.lastIndexOf("api"), path.length()-1) + " | Unexpected response code: " + statusCode + "\n");
+//		}
 	}
 		
 	public static String readHTMLHeader() {
