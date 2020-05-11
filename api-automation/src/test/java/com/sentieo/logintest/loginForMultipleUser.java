a95c72256a31f837d696293eb83976a930f510c7
Devesh Arora <devesh.arora@sentieo.com>
2020-05-12 02:54:31 +0530

    login case fix, incase single signon

--- api-automation/src/test/java/com/sentieo/logintest/loginForMultipleUser.java
+++ api-automation/src/test/java/com/sentieo/logintest/loginForMultipleUser.java
@@ -3,6 +3,7 @@ package com.sentieo.logintest;
 import static com.sentieo.constants.Constants.*;
 import java.util.HashMap;
 import org.json.JSONObject;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import com.jayway.restassured.response.Response;
@@ -12,19 +13,28 @@ import com.sentieo.assertion.APIAssertions;
 import com.sentieo.dataprovider.DataProviderClass;
 import com.sentieo.rest.base.APIResponse;
 import com.sentieo.rest.base.RestOperationUtils;
+import com.sentieo.utils.CoreCommonException;
 
 public class loginForMultipleUser extends APIDriver {
 
 	APIAssertions verify = new APIAssertions();
 	String userName;
 
-	@BeforeMethod
+	@BeforeMethod(alwaysRun = true)
 	public void setUp() {
 		verify = new APIAssertions();
 	}
 
+	@AfterClass(alwaysRun = true)
+	public void setCookieAgain() throws CoreCommonException {
+		APIDriver ap = new APIDriver();
+		ap.login();
+	}
+	
 	@Test(groups = "sanity", description = "check", dataProvider = "users", dataProviderClass = DataProviderClass.class)
 	public void verifyLogin(String email, String password) throws Exception {
+		apid="";
+		usid="";
 		String URI = USER_APP_URL + LOGIN_URL;
 		HashMap<String, String> loginData = new HashMap<String, String>();
 		loginData.put("email", email);
@@ -42,11 +52,9 @@ public class loginForMultipleUser extends APIDriver {
 			userName = respJson.getJSONObject("result").getString("email").toString();
 			verify.assertEqualsActualContainsExpected(email, userName, msg);
 		}
-
 		else {
 			verify.assertTrue(false, msg);
 		}
-
 		verify.verifyAll();
 	}
 
