ad1027289827e078e4c96f162bf36d501df00091
bhashkargaur <bhaskar@sentieo.com>
2020-10-12 23:35:51 +0530

    fix for plotter load

--- api-automation/src/test/java/com/sentieo/plotter/LoadGraph.java
+++ api-automation/src/test/java/com/sentieo/plotter/LoadGraph.java
@@ -28,14 +28,40 @@ public class LoadGraph extends APIDriver {
 	public void loadGraphNew(String sort_flag) throws Exception {
 		String URI = USER_APP_URL + LOADGRAPH_NEW;
 		if (URI.contains("app") || URI.contains("staging") || URI.contains("app2")) {
+			boolean load = true;
+			JSONObject loadData = null;
 			JSONArray graphobj = null;
 			HashMap<String, String> tickerData = new HashMap<String, String>();
 			tickerData.put("sort_flag", sort_flag);
 			try {
-				graphobj = loadGraphNew(tickerData);
-				if (graphobj.length() == 0 || graphobj == null)
-					verify.assertTrue(false, "Data is blank for : " + sort_flag);
+				RequestSpecification spec = formParamsSpec(tickerData);
+				Response resp = RestOperationUtils.post(URI, null, spec, tickerData);
+				APIResponse apiResp = new APIResponse(resp);
+				verify.verifyStatusCode(apiResp.getStatusCode(), 200);
+				JSONObject respJson = new JSONObject(apiResp.getResponseAsString());
+				verify.verifyResponseTime(resp, 5000);
+				verify.verifyEquals(respJson.getJSONObject("response").getBoolean("status"), true,
+						"Verify the API Response Status");
+				if (sort_flag.equalsIgnoreCase("tagged_series"))
+					loadData = respJson.getJSONObject("result").getJSONObject("graphobj");
 
+				else
+					graphobj = respJson.getJSONObject("result").getJSONArray("graphobj");
+
+				if (sort_flag.equalsIgnoreCase("tagged_series")) {
+					if (loadData.length() == 0 || loadData == null) {
+						verify.assertTrue(false, "graphobj data is blank for : " + sort_flag);
+						load = false;
+					}
+				} else {
+					if (graphobj.length() == 0 || graphobj == null) {
+						verify.assertTrue(false, "graphobj data is blank for : " + sort_flag);
+						load = false;
+					}
+				}
+
+				if (load)
+					verify.assertTrue(load, "verify data for : " + sort_flag);
 			} catch (Exception e) {
 				verify.verificationFailures.add(e);
 				ExtentTestManager.getTest().log(LogStatus.FAIL, e.getMessage());
@@ -96,30 +122,6 @@ public class LoadGraph extends APIDriver {
 			// TODO: handle exception
 		}
 
-	}
-
-	public JSONArray loadGraphNew(HashMap<String, String> parameters) throws Exception {
-		String URI = USER_APP_URL + LOADGRAPH_NEW;
-		org.json.JSONArray graphobj = null;
-		try {
-			RequestSpecification spec = formParamsSpec(parameters);
-			Response resp = RestOperationUtils.post(URI, null, spec, parameters);
-			APIResponse apiResp = new APIResponse(resp);
-			System.out.println(apiResp.getStatusCode());
-			verify.verifyStatusCode(apiResp.getStatusCode(), 200);
-			JSONObject respJson = new JSONObject(apiResp.getResponseAsString());
-			verify.verifyResponseTime(resp, 5000);
-			verify.verifyEquals(respJson.getJSONObject("response").getBoolean("status"), true,
-					"Verify the API Response Status");
-			graphobj = respJson.getJSONObject("result").getJSONArray("graphobj");
-			return graphobj;
-
-		} catch (Exception e) {
-			verify.verificationFailures.add(e);
-			ExtentTestManager.getTest().log(LogStatus.FAIL, e.getMessage());
-		}
-		verify.verifyAll();
-		return graphobj;
 	}
 
 }
