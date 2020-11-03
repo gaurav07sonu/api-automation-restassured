668fc570c2d1f33f9e2fa77acc721b611c7b4feb
bhashkargaur <bhaskar@sentieo.com>
2020-11-03 23:12:06 +0530

    add check for date

--- api-automation/src/test/java/com/sentieo/plotter/TestDailySeriesData.java
+++ api-automation/src/test/java/com/sentieo/plotter/TestDailySeriesData.java
@@ -35,6 +35,7 @@ public class TestDailySeriesData extends APIDriver {
 
 	public void keyMultiples(String headName, String graphType, String ticker) throws Exception {
 		CommonUtil obj = new CommonUtil();
+		Calendar calNewYork = Calendar.getInstance();
 		String expectedDate = obj.getDate(0);
 		JSONArray value = null;
 		HashMap<String, String> parameters = new HashMap<String, String>();
@@ -100,15 +101,15 @@ public class TestDailySeriesData extends APIDriver {
 				int digit = (int) (timestamp / 1000);
 				CommonUtil util = new CommonUtil();
 				String date = util.convertTimestampIntoDate(digit);
-				if (!date.contains(expectedDate))
-					expectedDate = obj.getDate(-1);
-
-				if (!date.contains(expectedDate))
-					expectedDate = obj.getDate(-2);
-
-				if (!date.contains(expectedDate))
-					expectedDate = obj.getDate(-3);
-
+				int dayOfWeek = calNewYork.get(Calendar.DAY_OF_WEEK);
+				if (!date.contains(expectedDate)) {
+					if (dayOfWeek == 1)
+						expectedDate = obj.getDate(-2);
+					else if (dayOfWeek == 2)
+						expectedDate = obj.getDate(-3);
+					else
+						expectedDate = obj.getDate(-1);
+				}
 				verify.compareDates(date, expectedDate, "Verify the Current Date Point");
 			}
 		} else {
